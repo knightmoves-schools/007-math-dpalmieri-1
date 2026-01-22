98113674ab4c07d71a4aac150ec3361c857902ad
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-22 13:26:44 -0600

    Update Math.cs

--- Math.cs
+++ Math.cs
@@ -2,18 +2,17 @@
 public class Math
 {
     // Add your code here
-    public static string GotAllOfThem(){
+    public static string AllOfThem(){
         int Sum       = 1 + 4;
         int Difference = 5 - 2;
         int Product   = 3 * 5;
         int Quotient  = 15 / 2;
         int Remainder = 7 % 2;
 
-        string AllOfThem = $"{Sum}{Difference}{Product}{Quotient}{Remainder}";
-
-        return AllOfThem;
+        return $"{Sum}{Difference}{Product}{Quotient}{Remainder}";
     }
 }
+
 
 
 
