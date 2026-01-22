9058cf07bbd2bd5bc667924f13fa55381ffe2643
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-22 13:04:24 -0600

    Update Math.cs

--- Math.cs
+++ Math.cs
@@ -2,4 +2,10 @@
 public class Math
 {
     // Add your code here
+    int Sum = 1 + 4;
+    int Difference = 5 - 2;
+    int Product = 3 * 5;
+    int Quotient = 15 / 2;
+    int Remainder = 7 % 2;
 }
+
